8896e00c1352881ac8544bb0986e84186fb65220
oni128 <lyw283828@gmail.com>
2025-02-13 18:31:33 +0900

    [feat/이예원/트레이너 필드 작성]

--- src/main/java/com/gyupang/trainer/aggregate/Trainer.java
+++ src/main/java/com/gyupang/trainer/aggregate/Trainer.java
@@ -1,4 +1,16 @@
 package com.gyupang.trainer.aggregate;
 
+import java.time.LocalDate;
+
 public class Trainer {
+    private String id;                          // 트레이너 번호
+    private String name;                        // 이름(별명)
+    private String pwd;                         // 비밀번호
+    private LocalDate accountCreationDate;      // 계정 생성일
+    private LocalDate accountDeleationDate;     // 탈퇴 날짜
+    private int numberCaptured;                 // 잡은 포켓몬 수
+    private int win;                            // 배틀 이긴 횟수
+    private int lose;                           // 배틀 진 횟수
+    private String totalBattles;                // 배틀 총 전적
+
 }
