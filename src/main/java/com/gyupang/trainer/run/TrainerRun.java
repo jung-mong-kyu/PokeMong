b56917d5155bd5c4ad9b86c63817816490b52785
JK-LEE98 <abc9541@gmail.com>
2025-02-16 18:50:22 +0900

    회원가입 시 한글만 입력 가능 조건 추가

--- src/main/java/com/gyupang/trainer/run/TrainerRun.java
+++ src/main/java/com/gyupang/trainer/run/TrainerRun.java
@@ -11,13 +11,20 @@ public class TrainerRun {
     public static void signUp() {
         System.out.println("===== 회원가입 페이지다몽 =====");
         Scanner sc = new Scanner(System.in);
+
+        // 기존 한 글자 제약조건에 한글만 입력가능 제약조건 추가
+
         while (true) {
             System.out.print("트레이너 이름을 입력해달라몽 : ");
             String userName = sc.nextLine();
             if (userName.length() != 1) {
                 System.out.println("트레이너 이름은 한글자만 가능하다몽");
                 continue;
+            } else if (!userName.matches("^[가-힣]$")) {
+                System.out.println("트레이너 이름은 한글만 입력 가능하다몽");
+                continue;
             }
+
             while (true) {  // 비밀번호 제약 조건 추가 로직
                 System.out.print("트레이너 비밀번호를 입력해달라몽 : ");
                 String pwd = sc.nextLine();
